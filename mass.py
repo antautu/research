68858b8a2c04103e5b0ac69f30ca6d33f09999c4
antautu <antautu@lycoming.edu>
2016-06-29 16:56:23 -0400

    updates for all particles

--- mass.py
+++ mass.py
@@ -2,6 +2,7 @@ import glio
 s = glio.GadgetSnapshot('snapshot_000')
 s.load()
 
+
 f = open('data_gas_mass','w')
 for i in range(0, len(s.mass[0][:])):
 	f.write("%s\n" % s.mass[0][i])
@@ -9,8 +10,36 @@ for i in range(0, len(s.mass[0][:])):
 f.close()
 
 
+f = open('data_halo_mass','w')
+for i in range(0, len(s.mass[1][:])):
+	f.write("%s\n" % s.mass[1][i])
+
+f.close()
+
+
+f = open('data_disk_mass','w')
+for i in range(0, len(s.mass[2][:])):
+	f.write("%s\n" % s.mass[2][i])
+
+f.close()
+
+
+f = open('data_bulge_mass','w')
+for i in range(0, len(s.mass[3][:])):
+	f.write("%s\n" % s.mass[3][i])
+
+f.close()
+
+
 f = open('data_star_mass','w')
 for i in range(0, len(s.mass[4][:])):
 	f.write("%s\n" % s.mass[4][i])
+
+f.close()
+
+
+f = open('data_boundary_mass','w')
+for i in range(0, len(s.mass[5][:])):
+	f.write("%s\n" % s.mass[5][i])
 
 f.close()
